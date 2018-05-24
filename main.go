041543bc210bdbcd0a20e061ba70cb72a43f8515
Peter Marquardt <wwwutz@molgen.mpg.de>
2018-05-24 16:52:17 +0200

    filepath.Walk is a bad idea, back to the roots
    - recursion ?
    - sort ?
    
    OMG....

--- main.go
+++ main.go
@@ -7,44 +7,61 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/wwwutz/elwms"
 	"os"
-	"path/filepath"
 	"strconv"
 )
 
 func visit(path string, fi os.FileInfo, err error) error {
 	fmt.Printf("Visited: %s\n", path)
-
 	return nil
 }
 
-func main() {
+func readDirNames(dirname string) ([]string, error) {
+	d, err := os.Open(dirname)
+	if err != nil {
+		return nil, err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return nil, err
+	}
+	return names, nil
+}
 
-	database, _ := sql.Open("sqlite3", "omni-notes")
-	// statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+func main() {
+	database, err := sql.Open("sqlite3", "omni-notes")
+	elwms.Exiton(err, "sql.Open omni-notes failed")
+	defer database.Close()
+	// statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
 	// statement.Exec()
+	//        fmt.Printf("err: %#v",err)
 
 	flag.Parse()
 	root := "."
 	if flag.Arg(0) != "" {
 
 		fi, err := os.Stat(flag.Arg(0))
-//        fmt.Printf("err: %#v",err)
 		elwms.Exiton(err, "supplied arg "+flag.Arg(0)+" failed")
-        if !fi.IsDir() {
-            fmt.Printf("// "+flag.Arg(0)+" is not a directory\n")
-            os.Exit(1)
-        }
+		if !fi.IsDir() {
+			fmt.Printf("// " + flag.Arg(0) + " is not a directory\n")
+			os.Exit(1)
+		}
 		root = flag.Arg(0)
 	}
+	// readDirNames reads the directory named by dirname and returns
 
-	err := filepath.Walk(root, visit)
+	tree, err := readDirNames(root)
+	elwms.Exiton(err, "readDirNames("+root+") failed")
+	for i, entry := range tree {
+		fmt.Printf("-%d-> %s\n", i, entry)
+	}
 
-	fmt.Printf("filepath.Walk() returned %v\n", err)
-
-	statement, _ := database.Prepare("INSERT INTO notes (creation, last_modification, title, content) VALUES (?, ?, ?, ?)")
+	statement, err := database.Prepare("INSERT INTO notes (creation, last_modification, title, content) VALUES (?, ?, ?, ?)")
+	elwms.Exiton(err, "db.Prepare() failed")
 
 	statement.Exec(861, 10000000, "Total Recall", "krasser film")
-	rows, _ := database.Query("SELECT creation, title FROM notes")
+	rows, err := database.Query("SELECT creation, title FROM notes")
+	elwms.Exiton(err, "db.Query() failed")
 	var mID int
 	var title string
 	for rows.Next() {
